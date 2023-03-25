7a45fd93cfe4d36354aca4921df5c630ef323d59
arsiharsi <ahus981@gmail.com>
2023-03-25 13:27:16 +0500

    немного свагера
    Свагер в боте настроен

--- bot/src/main/java/app/BotController.java
+++ bot/src/main/java/app/BotController.java
@@ -1,9 +1,25 @@
 package app;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 @RestController
+
 public class BotController {
-    @RequestMapping("/")
+    @RequestMapping("/api_testing")
     String hello() {
         return "<html>Bot</html>";
     }
+    @GetMapping("/api_testing/{id}")
+    public String findById(@PathVariable long id) {
+        return id+"";
+    }
+
+
+    @PutMapping("/api_testing/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public String updateBook(
+            @PathVariable("id") final String id, @RequestBody final String ID) {
+        return ID;
+    }
+
+
 }
---
+++ bot/src/main/java/dto_classes/DataClass.java
@@ -0,0 +1,8 @@
+package dto_classes;
+
+public class DataClass {
+    private long id;
+    private String url;
+    private String description;
+    private long[] tgChatIds;
+}
