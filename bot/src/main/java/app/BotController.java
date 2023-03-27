b3bf38c211fc8d84f339ec9c88fe767f34c0d7d4
arsiharsi <ahus981@gmail.com>
2023-03-27 23:05:35 +0500

    Рабочая апишка
    Апишка прописана для бота и скраппера

--- bot/src/main/java/app/BotController.java
+++ bot/src/main/java/app/BotController.java
@@ -1,32 +1,36 @@
 package app;
+import dto_classes.ApiErrorResponse;
 import dto_classes.DataClass;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.core.codec.StringDecoder;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping(value = "/api_testing", method = RequestMethod.GET)
+@RequestMapping("/updates")
 @RestController
 public class BotController {
 
+    @ApiResponse(responseCode = "200", description = "Обработано")
+    @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса")
 
-    @GetMapping("/show/{id}")
-    public DataClass findById(@PathVariable int id) {
-        DataClass dataClass = new DataClass(id, "Common URL","Description",new int[1]);
-        return dataClass;
-    }
 
     @PostMapping(consumes = "application/json", produces = "application/json")
-    public DataClass addById(@RequestBody DataClass dataClass) {
-        return dataClass;
+    public DataClass update(@Valid @RequestBody DataClass dataClass) {
+        return new DataClass(dataClass.getId(), dataClass.getUrl(), dataClass.getDescription(), dataClass.getTgChatIds());
     }
 
-    @PutMapping(value = "/{id}", consumes = "application/json",produces = "application/json")
-    public DataClass setById(@RequestBody DataClass dataClass) {
-//        DataClass dClass = new DataClass(dataClass.getId(), dataClass.getUrl(), dataClass.getDescription(), dataClass.getTgChatIds());
-        return dataClass;
-    }
 
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    //@ExceptionHandler(MethodArgumentNotValidException.class)
+    public ApiErrorResponse handleException(MethodArgumentNotValidException e){
+        return new ApiErrorResponse("Некорректные параметры запроса",
+                e.getStatusCode().toString(),
+                e.getObjectName(),
+                e.getLocalizedMessage(),
+                new String[]{String.valueOf(e.getStackTrace())});
+    }
 
 }
--- bot/src/main/java/dto_classes/DataClass.java
+++ bot/src/main/java/dto_classes/DataClass.java
@@ -3,53 +3,23 @@ package dto_classes;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @Data
 public class DataClass {
-    public DataClass(int in_id, String in_url, String in_description, int[] in_tgChatIds){
-        id = in_id;
-        url = in_url;
-        description = in_description;
-        tgChatIds = in_tgChatIds;
-    }
-    public int getId() {
-        return id;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public int[] getTgChatIds() {
-        return tgChatIds;
-    }
-
-    public void setId(int id) {
+    public DataClass(@NotNull int id,@NotNull String url,@NotNull String description,@NotNull int[] tgChatIds){
         this.id = id;
-    }
-
-    public void setUrl(String url) {
         this.url = url;
-    }
-
-    public void setDescription(String description) {
         this.description = description;
-    }
-
-    public void setTgChatIds(int[] tgChatIds) {
         this.tgChatIds = tgChatIds;
     }
-
-    //@NotNull
+    @NotNull
     private int id;
-    //@NotNull
+    @NotNull
     private String url;
-   // @NotNull
+    @NotNull
     private String description;
-   // @NotNull
+    @NotNull
     private int[] tgChatIds;
 }
