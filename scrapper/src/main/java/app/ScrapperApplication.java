992427383d4ca3a8465e5aeba61fa9e7ee9fc8e2
arsiharsi <ahus981@gmail.com>
2023-04-02 15:50:33 +0500

    Исправлены 3 и 4 задания(не в том модуле сделал)

--- scrapper/src/main/java/app/ScrapperApplication.java
+++ scrapper/src/main/java/app/ScrapperApplication.java
@@ -5,7 +5,9 @@ import configuration.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
 public class ScrapperApplication {
