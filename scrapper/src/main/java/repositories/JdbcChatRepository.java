64d80fc7018078d3cfdb4e22a579658a1fa48bb3
arsiharsi <ahus981@gmail.com>
2023-04-26 00:15:17 +0500

    Финиш 6 и 7 дз
    Добавлены последние тесты
    Добавлено всё, что нужно для 7 дз

--- scrapper/src/main/java/repositories/JdbcChatRepository.java
+++ scrapper/src/main/java/repositories/JdbcChatRepository.java
@@ -1,11 +1,14 @@
 package repositories;
 
 import entity.Chat;
-import entity.Link;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.Repository;
 
-public interface LinkRepository extends Repository<Link, Long> {
-    Page<Link> findAll();
+import java.util.List;
+
+public interface JdbcChatRepository extends CrudRepository<Chat, Long> {
+    List<Chat> findAll();
+
 }
--- scrapper/src/main/java/repositories/JdbcLinkRepository.java
+++ scrapper/src/main/java/repositories/JdbcLinkRepository.java
@@ -1,11 +1,13 @@
 package repositories;
 
 import entity.Chat;
+import entity.Link;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.Repository;
 
-public interface ChatRepository extends Repository<Chat, Long> {
-    Page<Chat> findAll();
+import java.util.List;
 
+public interface JdbcLinkRepository extends Repository<Link, Long> {
+    List<Link> findAll();
 }
